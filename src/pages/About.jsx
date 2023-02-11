bb7008f43d3fa6fed928534e76fc6fefe20c0824
abdubosit-js <abdubositjs@gmail.com>
2023-02-11 13:35:55 +0500

    changed commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,135 @@
 import React from 'react'
 import styled from 'styled-components'
+import { ReactComponent as Arrow } from "../assets/Component 1.svg"
+import vrimage from "../assets/Mask group (1).png"
 
 export const About = () => {
     return (
         <Wrapper>
-            
+            <div className='top_text-cnt'>
+                <div className='left-text'>
+                    <h1>INTRODUCTION</h1>
+                    <div>
+                        <h2>TO HYDRA VR</h2>
+                        <Arrow />
+                    </div>
+                </div>
+                <div className='right-text'>
+                    <p>Vitae sapien pellentesque habitant morbi tristique senectus et netus et. Feugiat nibh sed pulvinar proin gravida hendrerit lectus. Mi sit amet mauris commodo quis imperdiet massa tincidunt nunc. Viverra aliquet eget sit amet tellus. Ornare lectus sit amet est placerat in. Lectus magna fringilla urna porttitor rhoncus vitae.</p>
+                </div>
+            </div>
+            <div className='bottom_img-cnt'>
+                <div className='img-cnt'>
+                    <img src={vrimage} alt="" />
+                </div>
+                <div className='vr_about-cnt'>
+                    <h1>ABOUT</h1>
+                    <h2>HYDRA VR</h2>
+                    <p>Eget mi proin sed libero enim sed faucibus turpis. Nisl rhoncus mattis rhoncus 
+                    urna neque viverra justo. Vivamus at augue eget arcu dictum. Ultrices gravida 
+                    dictum fusce ut placerat orci. Aenean et tortor at risus viverra adipiscing at in. 
+                    Mattis aliquam faucibus purus in massa. Est placerat in egestas erat imperdiet 
+                    sed. Consequat semper viverra nam libero justo laoreet sit amet. Aliquam 
+                    etiam erat velit scelerisque in dictum non consectetur a. Laoreet sit amet 
+                    cursus sit amet. Vel eros donec ac odio tempor orci dapibus. Sem nulla pha
+                    retra diam sit amet nisl suscipit adipiscing bibendum. Leo a diam sollicitudi
+                    n tempor.</p>
+                    <button>LET’S GET IN TOUCH</button>
+                </div>
+            </div>
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
+    padding: 0 84px;
+    .top_text-cnt {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .left-text {
+            h1 {
+                font-style: normal;
+                font-weight: 700;
+                font-size: 36px;
+                line-height: 44px;
+                color: #FFFFFF;
+            }
+        
+            div {
+                h2 {
+                    font-style: normal;
+                    font-weight: 300;
+                    font-size: 36px;
+                    line-height: 44px;
+                    color: #FFFFFF;
+                }
+                display: flex;
+                align-items: center;
+                gap: 10px;
+            }
+        }
+
+        .right-text {
+            p {
+                width: 652px;
+                font-style: normal;
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 20px;
+                color: #FFFFFF;
+            }
+        }
+    }
     
+    .bottom_img-cnt {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-top: 97px;
+        .vr_about-cnt {
+            h1 {
+                font-style: normal;
+                font-weight: 700;
+                font-size: 36px;
+                line-height: 44px;
+                color: #FFFFFF;
+                margin-top: -92px;
+            }
+            
+            h2 {
+                font-style: normal;
+                font-weight: 300;
+                font-size: 36px;
+                line-height: 44px;
+                color: rgba(255, 255, 255, 0.95);
+            }
+
+            p{
+                width: 630px;
+                font-style: normal;
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 20px;
+                color: #FFFFFF;
+                margin-top: 41px;
+            }
+
+            button {
+                margin-top: 28px;
+                width: 214px;
+                height: 48px;
+                left: 726px;
+                top: 1691px;
+                border: none;
+                background: linear-gradient(90deg, #8176AF 0%, #C0B7E8 100%);
+                border-radius: 40px;
+                font-style: normal;
+                font-weight: 700;
+                font-size: 12px;
+                line-height: 15px;
+                color: #343045;
+            }
+        }
+    }
 `
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,57 @@
 import React from 'react'
 import styled from 'styled-components'
 import { ReactComponent as Arrow } from "../assets/arrow-right.svg"
+import { ReactComponent as Location } from "../assets/Location-Icon.svg"
+import { ReactComponent as Mail } from "../assets/mail.svg"
+import { ReactComponent as Phone } from "../assets/phone-call.svg"
 import mask from "../assets/Mask group.png"
 
 export const Home = () => {
     return (
-        <Wrapper>
-            <div className='text-container'>
-                <h1><span>Dive</span> Into The Depths</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore 
-                nisl tincidunt eget. Lectus mauris eros in vitae .</p>
-                <div className='btn-flex'>
-                    <button>BUILD YOUR WORLD</button>
-                    <Arrow />
+        <>
+            <Wrapper>
+                <div className='text-container'>
+                    <h1><span>Dive</span> Into The Depths</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore 
+                    nisl tincidunt eget. Lectus mauris eros in vitae .</p>
+                    <div className='btn-flex'>
+                        <button>BUILD YOUR WORLD</button>
+                        <Arrow />
+                    </div>
                 </div>
-            </div>
-            <div className='image-container'>
-                <div>
-                    <img src={mask} alt="" />
+                <div className='image-container'>
+                    <div>
+                        <img src={mask} alt="" />
+                    </div>
                 </div>
-            </div>
-        </Wrapper>
+            </Wrapper>
+            <RectWrapper>
+                <div className='rect-container'>
+                    <div className='rext'>
+                        <Location/>
+                        <div className='text-cnt'>
+                            <h1>Pay Us a Visit</h1>
+                            <p>Union St, Seattle, WA 98101, United States</p>
+                        </div>
+                    </div>
+                    <div className='rext'>
+                        <Phone/>
+                        <div className='text-cnt'>
+                            <h1>Give Us a Call</h1>
+                            <p>(110) 1111-1010</p>
+                        </div>
+                    </div>
+                    <div className='rext'>
+                        <Phone/>
+                        <div className='text-cnt'>
+                            <h1>Send Us a Message</h1>
+                            <p>Contact@HydraVTech.com</p>
+                        </div>
+                    </div>
+                </div>
+            </RectWrapper>
+        </>
     )
 }
 
@@ -81,6 +111,42 @@ const Wrapper = styled.div`
             height: 455px;
             background: rgba(0, 0, 0, 0.13);
             border-radius: 105px 105px 105px 255px;
+        }
+    }
+`
+
+const RectWrapper = styled.div`
+    padding: 103px 84px;
+    .rect-container {
+        width: 100%;
+        height: 167px;
+        background: radial-gradient(50% 2900.76% at 50% 53.89%, rgba(58, 52, 86, 0.95) 0%, #211E2E 100%);
+        box-shadow: 0px 4px 4px rgba(192, 183, 232, 0.01);
+        border-radius: 83.5px;
+        display: flex; 
+        align-items: center;
+        justify-content: space-between;
+        padding: 50px 40px;
+        .rext {
+            display: flex;
+            align-items: center;
+            gap: 7px;
+            .text-cnt {
+                h1 {
+                    font-style: normal;
+                    font-weight: 700;
+                    font-size: 24px;
+                    line-height: 29px;
+                    color: #FFFFFF;
+                }
+                p {
+                    font-style: normal;
+                    font-weight: 400;
+                    font-size: 14px;
+                    line-height: 17px;
+                    color: #FFFFFF;
+                }
+            }
         }
     }
 `
